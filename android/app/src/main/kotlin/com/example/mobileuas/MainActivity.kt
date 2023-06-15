d81f09f87f1e8b285212d59736654de8da8a4f6e
Reilham RA <Reilham.ra@gmail.com>
2023-06-15 21:55:00 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/mobileuas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobileuas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/getstartedpage.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mobileuas/start.dart';
+
+class getstartedpage extends StatelessWidget {
+  const getstartedpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/cuci_sepatu 1.png"))),
+        ),
+        Container(
+          height: 444,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.black.withOpacity(0.9),
+              Colors.black.withOpacity(0.0),
+            ],
+          )),
+        ),
+        SafeArea(
+            child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("NEAT",
+                      style: GoogleFonts.poppins(
+                        fontSize: 40,
+                        color: Color(0xffFFFFFF),
+                        fontWeight: FontWeight.w800,
+                      )),
+                  Text(" SNEAKER",
+                      style: GoogleFonts.poppins(
+                        fontSize: 40,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontWeight: FontWeight.w800,
+                      )),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "I wish you was here Reviving Life to Your Shoes",
+              style: GoogleFonts.poppins(
+                color: Color(0xffFFFFFF),
+                fontSize: 16,
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Spacer(),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const StartPage()),
+                );
+              },
+              child: Center(
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 80),
+                  padding: EdgeInsets.symmetric(horizontal: 58, vertical: 14),
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 18, 152, 225),
+                      borderRadius: BorderRadius.circular(17)),
+                  child: Text("Get Started",
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontWeight: FontWeight.w500,
+                      )),
+                ),
+              ),
+            ),
+          ],
+        ))
+      ]),
+    );
+  }
+}
